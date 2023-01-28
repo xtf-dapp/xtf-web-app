89a25f3bf28f0d648e3dc7ffe1380033c33b6d3d
Smit <smitshah95@hotmail.com>
2023-01-29 03:06:45 +0800

    Updated chart of candle

--- src/components/trade/dydx/DyDxCandleChartComponent.tsx
+++ src/components/trade/dydx/DyDxCandleChartComponent.tsx
@@ -8,16 +8,16 @@ import React, { useEffect, useRef } from 'react';
 const DYDX_HOST = 'https://api.stage.dydx.exchange';
 
 const data = [
-    { time: '2018-12-22', value: 32.51 },
-    { time: '2018-12-23', value: 31.11 },
-    { time: '2018-12-24', value: 27.02 },
-    { time: '2018-12-25', value: 27.32 },
-    { time: '2018-12-26', value: 25.17 },
-    { time: '2018-12-27', value: 28.89 },
-    { time: '2018-12-28', value: 25.46 },
-    { time: '2018-12-29', value: 23.92 },
-    { time: '2018-12-30', value: 22.68 },
-    { time: '2018-12-31', value: 22.67 },
+    { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+    { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+    { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+    { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+    { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+    { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+    { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
 ];
 
 function DyDxCandleChartComponent(props: any) {
@@ -49,7 +49,7 @@ function DyDxCandleChartComponent(props: any) {
             });
             chart.timeScale().fitContent();
 
-            const newSeries = chart.addAreaSeries({ lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
+            const newSeries = chart.addCandlestickSeries();
             newSeries.setData(data);
 
             window.addEventListener('resize', handleResize);
