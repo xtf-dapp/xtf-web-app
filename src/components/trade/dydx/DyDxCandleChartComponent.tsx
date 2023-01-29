5cd4408799b68d8083d70a8511901e8331c58168
Smit <smitshah95@hotmail.com>
2023-01-29 12:01:09 +0800

    Updated chart component

--- src/components/trade/dydx/DyDxCandleChartComponent.tsx
+++ src/components/trade/dydx/DyDxCandleChartComponent.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useRef } from 'react';
 
 const DYDX_HOST = 'https://api.stage.dydx.exchange';
 
-const data = [
+const const_data = [
     { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
     { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
     { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
@@ -15,12 +15,11 @@ const data = [
     { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
     { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
     { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
 ];
 
 function DyDxCandleChartComponent(props: any) {
+    const [data, setData] = useState([]);
+
     const {
         colors: {
             backgroundColor = 'white',
@@ -54,6 +53,7 @@ function DyDxCandleChartComponent(props: any) {
 
             window.addEventListener('resize', handleResize);
 
+            load_data_in_time(newSeries)
             return () => {
                 window.removeEventListener('resize', handleResize);
 
@@ -63,9 +63,21 @@ function DyDxCandleChartComponent(props: any) {
         [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
     );
 
+    const load_data_in_time = (series: any) => {
+        setTimeout(() => {
+            series.update({ time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 })
+            setTimeout(() => {
+                series.update({ time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 })
+                setTimeout(() => {
+                    series.update({ time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 })
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }
+
     return (
         <>
-        <Navbar />
+            <Navbar />
             <div
                 ref={chartContainerRef}
             />
