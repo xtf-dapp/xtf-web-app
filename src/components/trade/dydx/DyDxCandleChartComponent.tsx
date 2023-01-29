fcba8bad72391b7548641d0f196066a362d78a77
Smit <smitshah95@hotmail.com>
2023-01-29 22:50:42 +0800

    Updated chart component to be accesible directly from navbar

--- src/components/trade/dydx/DyDxCandleChartComponent.tsx
+++ src/components/trade/dydx/DyDxCandleChartComponent.tsx
@@ -5,15 +5,10 @@ import { createChart, ColorType, OhlcData } from 'lightweight-charts';
 import React, { useEffect, useRef } from 'react';
 import { Button, Col, Form, Row } from "react-bootstrap";
 
-
-const DYDX_HOST = 'https://api.stage.dydx.exchange';
-
-var newSeries: any = null;
-
 function DyDxCandleChartComponent(props: any) {
-    const [data, setData] = useState<OhlcData[]>([]);
 
     const {
+        data,
         colors: {
             backgroundColor = 'white',
             lineColor = '#2962FF',
@@ -27,6 +22,7 @@ function DyDxCandleChartComponent(props: any) {
 
     useEffect(
         () => {
+            console.log("Inside of DyDxCandleChartComponent")
             const handleResize = () => {
                 chart.applyOptions({ width: chartContainerRef.current.clientWidth });
             };
@@ -41,12 +37,10 @@ function DyDxCandleChartComponent(props: any) {
             });
             chart.timeScale().fitContent();
 
-            newSeries = chart.addCandlestickSeries();
+            const newSeries = chart.addCandlestickSeries();
             newSeries.setData(data);
 
             window.addEventListener('resize', handleResize);
-
-            load_data_in_time()
             return () => {
                 window.removeEventListener('resize', handleResize);
 
@@ -56,36 +50,7 @@ function DyDxCandleChartComponent(props: any) {
         [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
     );
 
-    const convertDate = (date: string) => {
-        const dateObject = new Date(date);
-        const year = dateObject.getFullYear();
-        const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
-        const day = dateObject.getDate().toString().padStart(2, '0');
-        const formattedDate = `${year}-${month}-${day}`;
-        return formattedDate;
-    }
-
-    const load_data_in_time = () => {
-        const public_client = new DydxClient(DYDX_HOST);
-
-        public_client.public.getCandles({
-            market: props.market,
-            resolution: props.resolution,
-        }).then((response) => {
-            const candles = response.candles;
-
-            candles.sort((a, b) => new Date(a.startedAt).getTime() - new Date(b.startedAt).getTime())
-
-            const updatedData: OhlcData[] = [];
-
-            candles.forEach((item) => {
-                const ob = { time: convertDate(item.startedAt), open: Number(item.open), high: Number(item.high), low: Number(item.low), close: Number(item.close) } as OhlcData;
-                updatedData.push(ob);
-                newSeries.update(ob);
-            })
-            setData(updatedData);
-        })
-    }
+    
 
     return (
         <>
--- src/components/trade/dydx/DyDxChartComponent.tsx
+++ src/components/trade/dydx/DyDxChartComponent.tsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import Navbar from "../../navigation/NavigationComponent";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import DyDxCandleChartComponent from "./DyDxCandleChartComponent";
+import { createChart, ColorType, OhlcData } from 'lightweight-charts';
 
+const DYDX_HOST = 'https://api.stage.dydx.exchange';
 
 const markets: string[] = [Market.BTC_USD, Market.ETH_USD]
 
@@ -12,29 +14,68 @@ const resolutions: string[] = [CandleResolution.ONE_MIN, CandleResolution.FIFTEE
 function DyDxChartComponent(props: any) {
     const [market, setMarket] = useState<any>(Market.ETH_USD)
     const [resolution, setResolution] = useState<any>(CandleResolution.ONE_DAY)
+    const [data, setData] = useState<any>([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const updateMarket = (tarEnv: any) => {
-        console.log(tarEnv.target.value)
+        setMarket(tarEnv.target.value)
+        load_data_in_time()
     }
 
     const updateResolution = (tarEnv: any) => {
-        console.log(tarEnv.target.value)
+        setResolution(tarEnv.target.value)
+        load_data_in_time()
+    }
+
+    const convertDate = (date: string) => {
+        const dateObject = new Date(date);
+        const year = dateObject.getFullYear();
+        const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+        const day = dateObject.getDate().toString().padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+    }
+
+    const load_data_in_time = () => {
+        setIsLoading(true)
+        const public_client = new DydxClient(DYDX_HOST);
+
+        public_client.public.getCandles({
+            market: market,
+            resolution: resolution,
+        }).then((response) => {
+            const candles = response.candles;
+
+            candles.sort((a, b) => new Date(a.startedAt).getTime() - new Date(b.startedAt).getTime())
+
+            const updatedData: OhlcData[] = [];
+
+            candles.forEach((item) => {
+                const ob = { time: convertDate(item.startedAt), open: Number(item.open), high: Number(item.high), low: Number(item.low), close: Number(item.close) } as OhlcData;
+                updatedData.push(ob);
+            })
+            setData(updatedData);
+            setIsLoading(false)
+        }).catch((error: any) => {
+            console.log("error when getting candle data ", error);
+            setIsLoading(false)
+        })
     }
 
     return (
         <>
-            <Navbar currentNode="Chart"/>
+            <Navbar currentNode="Chart" />
             <Form>
                 <Row>
                     <Col>
                         <Form.Label>Market</Form.Label>
-                        <Form.Select value={market} onChange={(tarEnv) => updateMarket(tarEnv)}>
+                        <Form.Select value={market} onChange={(tarEnv) => updateMarket(tarEnv)} disabled={isLoading}>
                             {markets.map(item => (<option value={item}>{item}</option>))}
                         </Form.Select>
                     </Col>
                     <Col>
                         <Form.Label>Duration</Form.Label>
-                        <Form.Select value={resolution} onChange={(tarEnv) => updateResolution(tarEnv)}>
+                        <Form.Select value={resolution} onChange={(tarEnv) => updateResolution(tarEnv)} disabled={isLoading}>
                             {resolutions.map(item => (<option value={item}>{item}</option>))}
                         </Form.Select>
                     </Col>
@@ -42,7 +83,7 @@ function DyDxChartComponent(props: any) {
             </Form>
             <br />
             <br />
-            <DyDxCandleChartComponent market={market} resolution={resolution} />
+            <DyDxCandleChartComponent data={data} />
         </>
     );
 };
